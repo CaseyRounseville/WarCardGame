96aa5b75a34f10021c853af41e019edca3a6eb12
CaseyRounseville <mariarounseville@cox.net>
2015-01-15 08:22:17 -0700

    Update CardList.java

--- src/CardList.java
+++ src/CardList.java
@@ -1,1 +1,28 @@
 // create class CardList
+import java.util.ArrayList;
+import java.util.Random;
+public class CardList{
+  private ArrayList Card cards;
+  private Random random=new Random();
+  public CardList(){
+    cards=new ArrayList<Card>();
+    suit=random.nextInt(4);
+    rank=random.nextInt(cards.size());
+  }
+  public int size(){
+    return cards.size();
+  }   
+  public void addCardToBottom(){
+    /*
+     * fields:
+     *   cards
+     *   random
+     * constructors:
+     *   cardList()
+     * accessors:
+     *   size()
+     * mutators:
+     *   addCardToBottom
+     *   addCardToTop
+     *   takeCardFromTop
+     *   removeRandomCard
