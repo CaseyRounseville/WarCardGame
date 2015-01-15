93c4b0ada3b8be002333f3a9d35926a9481e0159
CaseyRounseville <mariarounseville@cox.net>
2015-01-15 08:21:48 -0700

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,15 @@
 // Complete Class card by extending abstract Class PlayingCard and implementing the Comparable interface
-
+import java.util.Random;
+public class Card extends PlayingCard implements Comparable{
+  public Card(){
+    /*suit=new Random.nextInt(4);
+    rank=new Random.nextInt(cards.size());*/
+  }
+}
+/*
+ * constructors:
+ *   card(int suit, int rank)
+ * accessors:
+ *   toString()
+ *   compareTo(...):comparable by rank
+  
