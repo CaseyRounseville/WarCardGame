48b4417b356d132b4d4651865120d25e87930203
CaseyRounseville <mariarounseville@cox.net>
2015-01-16 00:17:42 -0700

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,15 +1,63 @@
 // Complete Class card by extending abstract Class PlayingCard and implementing the Comparable interface
-import java.util.Random;
-public class Card extends PlayingCard implements Comparable{
-  public Card(){
-    /*suit=new Random.nextInt(4);
-    rank=new Random.nextInt(cards.size());*/
+public class Card extends PlayingCard implements Comparable<Card>
+{
+  public Card(int suit,int rank)
+  {
+    super(suit,rank);
+  }
+  public int compareTo(Card other)
+  {
+    if(this.getRank()>other.getRank())
+    {
+      return 1;
+    }
+    if(this.getRank()<other.getRank())
+    {
+      return -1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  public String toString()
+  {
+    String rank="";
+    String suit="";
+    if(getRank()==11)
+    {
+      rank="J";
+    }
+    if(getRank()==12)
+    {
+      rank="Q";
+    }
+    if(getRank()==13)
+    {
+      rank="K";
+    }
+    if(getRank()==1)
+    {
+      rank="A";
+    }
+    if(getSuit()==1)
+    {
+      suit="H";
+    }
+    if(getSuit()==2)
+    {
+      suit="C";
+    }
+    if(getSuit()==3)
+    {
+      suit="D";
+    }
+    if(getSuit()==4)
+    {
+      suit="S";
+    }
+    return rank+"-"+suit;
   }
 }
-/*
- * constructors:
- *   card(int suit, int rank)
- * accessors:
- *   toString()
- *   compareTo(...):comparable by rank
+
   
